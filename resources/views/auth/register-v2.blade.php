449b17b71d4cb039d3ceddfc5c3119d6f58d2020
DonAbenz <xbossabenz@gmail.com>
2023-06-13 00:26:47 +0800

    register view field placeholder

--- resources/views/auth/register-v2.blade.php
+++ resources/views/auth/register-v2.blade.php
@@ -65,7 +65,7 @@
                         <label for="name" class="sr-only">Name</label>
                         <input id="name" name="name" type="text" autocomplete="name" :value="old('name')"
                             class="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-[#4F4537] sm:text-sm sm:leading-6"
-                            placeholder="name">
+                            placeholder="Name">
                     </div>
                     <div>
                         <label for="email-address" class="sr-only">Email address</label>
